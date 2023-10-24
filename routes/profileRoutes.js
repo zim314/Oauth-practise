8a77305957cda0941a54f3bd506a95d458a9e67a
zim <qwe1472571@gmail.com>
2023-10-24 11:35:47 +0800

    新增 登入驗證

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,7 +1,10 @@
 import express from 'express';
 
 const router = express.Router();
+const authCheck = (req, res, next) => {
+    req.isAuthenticated()? next() : res.redirect('/auth/login');
+};
 
-router.get('/', (req, res) => res.render('profile', { user: req.user }));
+router.get('/', authCheck, (req, res) => res.render('profile', { user: req.user }));
 
 export default router;
