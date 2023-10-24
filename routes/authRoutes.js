96667f207874e2d51752823fe9b3ed77b598504e
zim <qwe1472571@gmail.com>
2023-10-24 15:45:29 +0800

    新增 本地註冊帳號

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,6 +2,7 @@ import express from 'express';
 import passport from 'passport';
 import { Strategy as googleStrategy }  from 'passport-google-oauth20';
 import User from '../models/user.js';
+import bcrypt from 'bcrypt';
 
 passport.serializeUser((user, done) => done(null, user._id));
 passport.deserializeUser(async (_id, done) => {
@@ -42,19 +43,29 @@ router.get('/logout', (req, res) => req.logOut(() => res.redirect('/')));
 router.get('/signup', (req, res) => res.render('signup', { user: req.user }));
 
 router.post('/signup', async (req, res) => {
-    const { name, password, email } = req.body;
-
-    if(password.length < 8) {
-        req.flash('error_message', '密碼過短 請輸入8~12位的英文或數字');
+    try {
+        const { name, password, email } = req.body;
+        const verifyEmail = await User.findOne({ email }).exec();
+            
+        if(verifyEmail) {
+            req.flash('error_message', '此信箱已被註冊，請使用其他信箱');
+            res.redirect('/auth/signup');
+        } 
+        if(password.length < 8) {
+            req.flash('error_message', '密碼過短，請輸入8~12位的英文或數字');
+            res.redirect('/auth/signup');
+        } 
+        if(password.length > 12) {
+            req.flash('error_message', '密碼過長，請輸入8~12位的英文或數字');
+            res.redirect('/auth/signup');
+        }
+        
+        const hashPassword = await bcrypt.hash(password, 12);
+        const newUser = new User({ name, hashPassword, email });
+        newUser.save();
+        req.flash('success_message', '恭喜註冊成功，現在可以開始使用POST系統！');
         res.redirect('/auth/signup');
-    } 
-
-    if(password.length > 12) {
-        req.flash('error_message', '密碼過長 請輸入8~12位的英文或數字')
-        res.redirect('/auth/signup');
-    };
-
-
+    } catch {() => console.log('註冊錯誤')} 
 });
 
 router.get('/google', passport.authenticate('google', 
